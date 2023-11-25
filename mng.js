cab46cfc2b3e9f0bb9e575268a340249d57f2e95
alon <72901838+Aloncheeto@users.noreply.github.com>
2023-11-25 14:35:05 -0300

    Karamino

---
+++ mng.js
@@ -0,0 +1,2 @@
+function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode, volume, language ) {}
+function SetFilesTotal( total ) {document.getElementById("dummy1").innerHTML = total}
